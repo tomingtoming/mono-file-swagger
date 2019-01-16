27e5a0c48f9b40d985ddd1ed871b7a7d921ca615
toming <tomingtoming@gmail.com>
2019-01-17 07:37:00 +0900

    succeeded to split

--- resolve.js
+++ resolve.js
@@ -2,52 +2,64 @@
 
 'use strict';
 
-var YAML = require('js-yaml');
-var fs = require('fs');
-var jp = require('jsonpath');
+const YAML = require('js-yaml');
+const fs = require('fs');
+const jp = require('jsonpath');
 
-var program = require('commander');
+const program = require('commander');
 
 program
-  .version('0.0.1')
-  .option('-o --output-format [output]',
-          'output format. Choices are "json" and "yaml"',
-          'json')
-  .option('-s --split-format [split]',
-          'split format. Specify comma separated JSONPath',
-          '$.paths.*,$.components.schemas.*')
-  .usage('[options] <yaml file> <target directory>')
-  .parse(process.argv);
+    .version('0.0.1')
+    .option('-o --output-format [output]',
+        'output format. Choices are "json" and "yaml"',
+        'json')
+    .option('-s --split-format [split]',
+        'split format. Specify comma separated JSONPath',
+        '$.paths.*,$.components.schemas.*')
+    .usage('[options] <yaml file> <target directory>')
+    .parse(process.argv);
 
 if (program.outputFormat !== 'json' && program.outputFormat !== 'yaml') {
-  console.error(program.help());
-  process.exit(1);
+    console.error(program.help());
+    process.exit(1);
 }
 
-var file = program.args[0];
+const file = program.args[0];
 
 if (!fs.existsSync(file)) {
-  console.error('File does not exist. ('+file+')');
-  process.exit(1);
+    console.error('File does not exist. (' + file + ')');
+    process.exit(1);
 }
 
-var directory = program.args[1];
+const directory = program.args[1];
 
 if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory);
 }
 
-var root = YAML.safeLoad(fs.readFileSync(file).toString());
-var jsonPaths = program.splitFormat.split(",");
+const root = YAML.safeLoad(fs.readFileSync(file).toString());
+const jsonPaths = program.splitFormat.split(",");
 
+function replace(obj, path, replaced) {
+    if (path.length === 1) {
+        obj[path[0]] = replaced;
+    } else {
+        replace(obj[path[0]], path.slice(1), replaced);
+    }
+}
 
-var a = (root, jsonPaths) => {
+function resolve(root, jsonPaths) {
+    jsonPaths.forEach(jsonPath => {
+        jp.paths(root, jsonPath).forEach(matchPath => {
+            replace(root, matchPath.slice(1), {"$ref": "replaced"});
+        });
+    });
     return root;
-};
+}
 
-var index = a(root, jsonPaths);
+const index = resolve(root, jsonPaths);
 if (program.outputFormat === 'yaml') {
-  fs.writeFileSync(directory + '/index.yaml', YAML.safeDump(index));
+    fs.writeFileSync(directory + '/index.yaml', YAML.safeDump(index));
 } else if (program.outputFormat === 'json') {
-  fs.writeFileSync(directory + '/index.yaml', JSON.stringify(index, null, 2));
+    fs.writeFileSync(directory + '/index.yaml', JSON.stringify(index, null, 2));
 }
