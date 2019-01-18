733ced1b6fc3aaf00ec843dc80894b227fe8ea10
toming <tomingtoming@gmail.com>
2019-01-18 09:33:54 +0900

    fix package.json: wrong binary name

--- resolve.js
+++ resolve.js
@@ -10,7 +10,7 @@ const jp = require('jsonpath');
 const program = require('commander');
 
 program
-    .version('0.0.1')
+    .version('1.0.1')
     .option('-o --output-format [output]',
         'output format. Choices are "json" and "yaml"',
         'json')
